927ecd9aae291591d441c6161106b46f8956ff65
Alexander Sierra <alexandercsierra@gmail.com>
2019-12-22 23:06:48 -0500

    Updates

---
+++ precache-manifest.a705147a29c68956e066e667f2a6d1cc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad105a39b8f179f1e8505ac1aea1279",
+    "url": "/christmas2019v2/index.html"
+  },
+  {
+    "revision": "63f9e85ab5d778dc4fba",
+    "url": "/christmas2019v2/static/css/main.fa6ecc01.chunk.css"
+  },
+  {
+    "revision": "b8b5d110461bad07e5a4",
+    "url": "/christmas2019v2/static/js/2.b26b9a59.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/christmas2019v2/static/js/2.b26b9a59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63f9e85ab5d778dc4fba",
+    "url": "/christmas2019v2/static/js/main.eb56014b.chunk.js"
+  },
+  {
+    "revision": "b78983fbb93b39ab9fe3",
+    "url": "/christmas2019v2/static/js/runtime-main.54bd79e6.js"
+  },
+  {
+    "revision": "3a170548ad351ac3fb24db79fee92a4d",
+    "url": "/christmas2019v2/static/media/back.3a170548.jpg"
+  },
+  {
+    "revision": "8eaa606d80232c1da2290c37d9f0a0fe",
+    "url": "/christmas2019v2/static/media/hannukard.8eaa606d.jpg"
+  },
+  {
+    "revision": "0c122b51aa6308a19a9b88e729b8d606",
+    "url": "/christmas2019v2/static/media/holidaycard.0c122b51.jpg"
+  },
+  {
+    "revision": "872d26c181c63b81bd6c8b43e0b09b8e",
+    "url": "/christmas2019v2/static/media/lights.872d26c1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/christmas2019v2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f21ea0b9b7cb8c3e57d58d080bf7d5d",
+    "url": "/christmas2019v2/static/media/stars.7f21ea0b.jpg"
+  },
+  {
+    "revision": "9409dffe572fe7ca12a194d97688bd2d",
+    "url": "/christmas2019v2/static/media/wood.9409dffe.jpg"
+  }
+]);
